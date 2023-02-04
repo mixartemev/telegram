4bd67c834f902f8119a33b9c35c144791144de29
artemiev <mixartemev@gmail.com>
2023-02-04 11:05:10 +0700

    Updates

---
+++ precache-manifest.b0eb23ac08d1b64dd62dc593b91caaff.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a2746dea0f4c5e7817d137673e0e01f",
+    "url": "/telegram/index.html"
+  },
+  {
+    "revision": "a707935c0a1cfd5b4e76",
+    "url": "/telegram/static/css/0.52c23fd6.chunk.css"
+  },
+  {
+    "revision": "e7be56e5426f71774a9e",
+    "url": "/telegram/static/css/4.4659485a.chunk.css"
+  },
+  {
+    "revision": "6c294880391faf49dc83",
+    "url": "/telegram/static/css/5.c61ffb77.chunk.css"
+  },
+  {
+    "revision": "0a2d9e083b7d5a9f3a8b",
+    "url": "/telegram/static/css/7.751a6c49.chunk.css"
+  },
+  {
+    "revision": "2e455ccdaa1ae44b3e48",
+    "url": "/telegram/static/css/main.162c7196.chunk.css"
+  },
+  {
+    "revision": "a707935c0a1cfd5b4e76",
+    "url": "/telegram/static/js/0.2719328c.chunk.js"
+  },
+  {
+    "revision": "f77df77b0064a06a0ced",
+    "url": "/telegram/static/js/3.c014426f.chunk.js"
+  },
+  {
+    "revision": "e7be56e5426f71774a9e",
+    "url": "/telegram/static/js/4.8eaa7943.chunk.js"
+  },
+  {
+    "revision": "6c294880391faf49dc83",
+    "url": "/telegram/static/js/5.9806e3f0.chunk.js"
+  },
+  {
+    "revision": "7d90f731adb96c7b86e3",
+    "url": "/telegram/static/js/6.91934930.chunk.js"
+  },
+  {
+    "revision": "0a2d9e083b7d5a9f3a8b",
+    "url": "/telegram/static/js/7.aa535dd2.chunk.js"
+  },
+  {
+    "revision": "2e455ccdaa1ae44b3e48",
+    "url": "/telegram/static/js/main.23f23a44.chunk.js"
+  },
+  {
+    "revision": "c3a6242aff2c44d61575",
+    "url": "/telegram/static/js/runtime~main.53f4b425.js"
+  },
+  {
+    "revision": "85479cc199de7ace1a44d901720601b8",
+    "url": "/telegram/static/media/bubble-tail-left.85479cc1.svg"
+  },
+  {
+    "revision": "bcf112461b322d3b888e4eb550e3386a",
+    "url": "/telegram/static/media/camomile_blurred.bcf11246.jpg"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/telegram/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "ac1331a490a116a48daa6c9f41b6db80",
+    "url": "/telegram/static/media/telegram-logo.ac1331a4.svg"
+  }
+]);
